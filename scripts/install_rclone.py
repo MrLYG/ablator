83352674e807869cb32c471177ce6bbe819eb12a
liyuangang <1095350436@qq.com>
2023-06-22 16:32:13 -0700

    Revert "Update install_rclone.py"
    This reverts commit 0c05c440c57e9bc7d674294bd59a9142e62bfd04.

--- scripts/install_rclone.py
+++ scripts/install_rclone.py
@@ -2,6 +2,7 @@ import os
 import sys
 import platform
 import urllib.request
+
 import zipfile
 import glob
 import shutil
@@ -10,54 +11,59 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-PROJECT_PATH = os.path.abspath(os.path.join(CURRENT_DIR, "..", "ablator"))
-PATH_EXTRACT_ZIP_TO = os.path.abspath(os.path.join(CURRENT_DIR, "..", "rclone_installing"))
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+PROJECT_PATH = os.path.join(CURRENT_DIR, "..",  "ablator/")
+PATH_EXTRACT_ZIP_TO = os.path.join(CURRENT_DIR, "..",  "rclone_installing/")
 
 
-def get_system_architecture():
-    system = platform.system().lower()
-    machine = platform.machine()
-
+def get_rclone_download_url(system, machine, beta=False):
+    """Returns the download url for rclone based on the OS type and architecture
+    """
     if system == "windows":
-        arch = "amd64" if machine == "AMD64" else "386"
-    elif system in ["darwin", "linux"]:
+        arch = "amd64" if machine in ["AMD64"] else "386"
+    elif system == "darwin":
+        system = "osx"
+        arch = "amd64" if machine in ["x86_64", "amd64"] else "386"
+    elif system == "linux":
         arch = "amd64" if machine in ["x86_64", "amd64"] else "386"
     else:
-        logging.error("Unsupported OS type")
+        print("OS type not supported")
         sys.exit(2)
 
-    return system, arch
-
-
-def get_rclone_download_url(system, arch, beta=False):
-    version = "beta" if beta else "current"
-    rclone_zip = f"rclone-{version}-latest-{system}-{arch}.zip"
-    download_url = f"https://downloads.rclone.org/{rclone_zip}" if not beta else f"https://beta.rclone.org/{rclone_zip}"
+    if beta:
+        download_url = f"https://beta.rclone.org/rclone-beta-latest-{system}-{arch}.zip"
+        rclone_zip = f"rclone-beta-latest-{system}-{arch}.zip"
+    else:
+        download_url = f"https://downloads.rclone.org/rclone-current-{system}-{arch}.zip"
+        rclone_zip = f"rclone-current-{system}-{arch}.zip"
 
     return download_url, rclone_zip
 
 
 def find_rclone(known_part, unknown_part):
-    pattern = os.path.join(known_part, unknown_part)
+    logging.info(f"Searching for rclone in {known_part}")
+    pattern = known_part + unknown_part
     matches = glob.glob(pattern)
-    return matches[0] if matches else None
+    return matches[0]
 
 
-def install_rclone(binary_path, destination_path):
+def install_rclone_on_windows(path_extract_to):
     try:
-        shutil.move(binary_path, destination_path)
+        shutil.move(find_rclone(path_extract_to, "*/rclone.exe"), f"{PROJECT_PATH}//rclone.exe")
     except PermissionError:
-        logging.error("Permission denied. You might need to run this script as Administrator." if platform.system().lower() == "windows" else "Permission denied. You might need to run this script as root.")
-        sys.exit(1)
+        logging.error("Permission denied. You might need to run this script as Administrator.")
 
 
-def make_executable(file_path):
-    if platform.system().lower() != "windows":
-        os.chmod(file_path, 0o755)
+def install_rclone_on_Linux(path_extract_to):
+    try:
+        shutil.move(find_rclone(path_extract_to, "*/rclone"), f"{PROJECT_PATH}/rclone")
+        os.chmod(f"{PROJECT_PATH}/rclone", 0o755)
+    except PermissionError:
+        logging.error("Permission denied. You might need to run this script as root.")
 
 
 def create_rclone_config(rclone_env_path):
+    dir_path = rclone_env_path
     config_filename = "rclone.conf"
     config = {
         "gcs": {
@@ -72,8 +78,8 @@ def create_rclone_config(rclone_env_path):
     }
     config_content = '\n'.join(f'[{name}]\n' + '\n'.join(f'{k} = {v}' for k, v in settings.items()) for name, settings in config.items())
 
-    os.makedirs(rclone_env_path, exist_ok=True)
-    config_path = os.path.join(rclone_env_path, config_filename)
+    os.makedirs(dir_path, exist_ok=True)
+    config_path = os.path.join(dir_path, config_filename)
     # Why I create a file to set config rather than using tempfiler.NamedTemporaryFile
     # Cause it has bug on windows, see https://bugs.python.org/issue14243
     with open(config_path, "w") as f:
@@ -81,48 +87,49 @@ def create_rclone_config(rclone_env_path):
     logging.info(f"Rclone configuration file has been written to {config_path}")
 
 
-def cleanup(files_and_dirs_to_remove):
-    for item in files_and_dirs_to_remove:
-        if os.path.exists(item):
-            if os.path.isfile(item):
-                os.remove(item)
-            else:
-                shutil.rmtree(item)
-
-
 def download_and_install_rclone(beta=False):
-    # Get system and architecture information
-    system, arch = get_system_architecture()
+    logging.info(f"CURRENT_DIR: {CURRENT_DIR}")
+    logging.info(f"PROJECT_PATH: {PROJECT_PATH}")
+    logging.info(f"PATH_EXTRACT_ZIP_TO: {PATH_EXTRACT_ZIP_TO}")
+
+    """Downloads and installs rclone based on the OS type and architecture"""
+    logging.info('download_and_install_rclone started')
+    system = platform.system().lower()
+    machine = platform.machine()
 
     # Get download url and zip file name
-    download_url, rclone_zip = get_rclone_download_url(system, arch, beta)
+    download_url, rclone_zip = get_rclone_download_url(system, machine, beta)
 
-    # Download rclone zip file
-    urllib.request.urlretrieve(download_url, rclone_zip)
+    # Download rclone zip file and save it to "rclone_zip" directory
+    logging.info(f"download_url: {download_url}")
+    print(f"Downloading rclone from {download_url}...")
+    if not os.path.exists(rclone_zip):
+        urllib.request.urlretrieve(download_url, rclone_zip)
 
-    # Extract the rclone zip file
+    # Unzip rclone zip file
+    logging.info(f"Unzipping {rclone_zip}...")
+
+    path_extract_to = PATH_EXTRACT_ZIP_TO
     with zipfile.ZipFile(rclone_zip, 'r') as zip_ref:
-        zip_ref.extractall(PATH_EXTRACT_ZIP_TO)
+        zip_ref.extractall(path_extract_to)
 
-    # Install rclone binary
-    rclone_binary = find_rclone(PATH_EXTRACT_ZIP_TO, "*/rclone.exe" if system == "windows" else "*/rclone")
-    if not rclone_binary:
-        logging.error("No rclone binary found in the downloaded package.")
-        sys.exit(1)
-
-    install_rclone(rclone_binary, os.path.join(PROJECT_PATH, "rclone.exe" if system == "windows" else "rclone"))
-
-    # Make rclone binary executable
-    make_executable(os.path.join(PROJECT_PATH, "rclone.exe" if system == "windows" else "rclone"))
+    # Move rclone binary to appropriate directory
+    if system == "windows":
+        install_rclone_on_windows(path_extract_to)
+    else:
+        install_rclone_on_Linux(path_extract_to)
 
     # Create rclone default configuration file
+    logging.info("Creating rclone configuration file...")
     create_rclone_config(PROJECT_PATH)
 
-    # Clean up downloaded and extracted files
-    cleanup([rclone_zip, PATH_EXTRACT_ZIP_TO])
+    # Clean up
+    logging.info("Cleaning up...")
+    shutil.rmtree(path_extract_to)
+    os.remove(rclone_zip)
 
 
 if __name__ == "__main__":
     download_and_install_rclone()
-    logging.info('Rclone installation completed successfully.')
-    sys.exit(0)
+    logging.info('download_and_install_rclone Finsihed')
+    exit(0)
